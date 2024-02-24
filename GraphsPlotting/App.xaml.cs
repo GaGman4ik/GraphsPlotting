da45cbf20b463a76fe9a571da015083c076d07ab
User <User@OLDXEONPC>
2024-02-24 17:46:28 +0300

    Add project files.

---
+++ GraphsPlotting/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GraphsPlotting
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
