30d3d0d68643a0f7139b71712f1262d1860a3d20
harch.andr <harch.andr131@gmail.com>
2024-02-28 23:04:00 +0300

    #Parcing and plotting in WPF

--- GraphsPlotting/MainWindow.xaml.cs
+++ GraphsPlotting/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZedGraph;
+
 
 namespace GraphsPlotting
 {
@@ -19,6 +21,102 @@ namespace GraphsPlotting
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        //double minX = 100000;
+        //double maxX = -100000;
+
+        //double minY = 100000;
+        //double maxY = -100000;
+
+        //double MapFromPixel(double pixelV, double pixelMax, double minV) =>
+        //    minV + (pixelV / pixelMax) * (maxX - minX);
+
+        //double MapToPixel(double v, double minV, double maxV, double pixelMax) =>
+        //    (v - minV) / (maxV - minV) * pixelMax;
+
+        private void BtnPrint_Click(object sender, RoutedEventArgs e)
+        {
+
+            //minX = Double.Parse(TextBoxDownBound.Text);
+            //maxX = Double.Parse(TextBoxUpBound.Text);
+            Parcer parser = new Parcer(TextBoxInput.Text);
+            var result = parser.Parse();
+            //var pixelWidth = Graph.ActualWidth;
+            //var pixelHeight = Graph.ActualHeight;
+            //PointCollection points = new PointCollection((int)pixelWidth + 1);
+            //for (double pixelX = minX; pixelX < maxX; pixelX += (maxX - minX) / pixelWidth)
+            //{
+            //    var y = parser.Calculate(result, pixelX);
+            //    minY = Math.Min(minY, y);
+            //    maxY = Math.Max(maxY, y);
+            //}
+            //for (int pixelX = 0; pixelX < pixelWidth; pixelX++)
+            //{
+            //    var x = MapFromPixel(pixelX, pixelWidth, minX);
+            //    var y = parser.Calculate(result, x);
+            //    var pixelY = pixelHeight - MapToPixel(y, minY, maxY, pixelHeight);
+            //    points.Add(new System.Windows.Point(pixelX, pixelY));
+            //}
+            //Graph.Points = points;
+
+            // Получим панель для рисования
+            GraphPane pane = zgc.GraphPane;
+
+            // Очистим список кривых на тот случай, если до этого сигналы уже были нарисованы
+            pane.CurveList.Clear();
+
+            // Создадим список точек
+            PointPairList list = new PointPairList();
+
+            double xmin = Double.Parse(TextBoxDownBound.Text);
+            double xmax = Double.Parse(TextBoxUpBound.Text);
+
+            // Заполняем список точек
+            for (double x = xmin; x <= xmax; x += (xmax - xmin)/100000)
+            {
+                // добавим в список точку
+                list.Add(x, parser.Calculate(result, x));
+            }
+
+            // Создадим кривую с названием "Sinc",
+            // которая будет рисоваться голубым цветом (Color.Blue),
+            // Опорные точки выделяться не будут (SymbolType.None)
+            LineItem myCurve = pane.AddCurve("Sinc", list, System.Drawing.Color.Blue, SymbolType.None);
+
+            // Вызываем метод AxisChange (), чтобы обновить данные об осях.
+            // В противном случае на рисунке будет показана только часть графика,
+            // которая умещается в интервалы по осям, установленные по умолчанию
+            zgc.AxisChange();
+
+            // Обновляем график
+            zgc.Invalidate();
+        }
+
+        private void zedGraph_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            // Сюда будут записаны координаты в системе координат графика
+            double x, y;
+
+            // Пересчитываем пиксели в координаты на графике
+            // У ZedGraph есть несколько перегруженных методов ReverseTransform.
+            zgc.GraphPane.ReverseTransform(e.Location, out x, out y);
+
+            // Выводим результат
+            string text = string.Format("X: {0};    Y: {1}", x, y);
+            zgc.Text = text;
+        }
+
+
+        private void TextBoxInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void WindowsFormsHost_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+            
         }
     }
 }
---
+++ GraphsPlotting/Parser.cs
@@ -0,0 +1,207 @@
+﻿using System;
+
+namespace GraphsPlotting
+{
+    class Expression
+    {
+        public string Token;
+        //public Expression arg1;
+        //public Expression arg2;
+        public Expression[] Args = new Expression[0];
+        public Expression(string token) { this.Token = token; }
+        public Expression(string token, Expression a)
+        {
+            this.Token = token;
+            this.Args = new Expression[1];
+            this.Args[0] = a;
+        }
+        public Expression(string token, Expression a, Expression b)
+        {
+            this.Token = token;
+            this.Args = new Expression[2];
+            this.Args[0] = a;
+            this.Args[1] = b;
+        }
+    }
+    class Parcer
+    {
+        private string _input;
+        private int _i;
+        public Parcer(string expression)
+        {
+            _input = expression;
+        }
+
+        private string Parse_token()
+        {
+            if (_i > _input.Length - 1) return "";
+
+            while (_input[_i] == ' ')
+                if (_i < _input.Length - 1) ++_i; else break;
+
+            if (Char.IsDigit(_input, _i))
+            {
+                string number = "";
+                while (Char.IsDigit(_input, _i) || _input[_i] == '.')
+                {
+                    number += _input[_i];
+                    ++_i;
+                    if (_i > _input.Length - 1)
+                        break;
+                }
+                return number;
+            }
+
+            switch (_input[_i])
+            {
+                case 'x':
+                    ++_i;
+                    return "x";
+                case '+':
+                    ++_i;
+                    return "+";
+                case '-':
+                    ++_i;
+                    return "-";
+                case '*':
+                    ++_i;
+                    return "*";
+                case '/':
+                    ++_i;
+                    return "/";
+                case '^':
+                    ++_i;
+                    return "^";
+                case 's':
+                    if (_input[_i + 1] == 'i')
+                    {
+                        _i += 2;
+                        goto case 'n';
+                    }
+                    if (_input[_i + 1] == '(')
+                    {
+                        ++_i;
+                        return "cos";
+                    }
+                    break;
+                case 'n':
+                    if (_input[_i] == 'n')
+                    {
+                        ++_i;
+                        return "sin";
+                    }
+                    break;
+
+                case 'c':
+                    if (_input[_i + 1] == 'o' && _input[_i + 2] == 's')
+                        _i += 3;
+                    return "cos";
+
+                case '(':
+                    ++_i;
+                    return "(";
+                case ')':
+                    ++_i;
+                    return ")";
+            }
+            return "";
+        }
+
+
+        private Expression Parse_simple_expression()
+        {
+            var token = Parse_token();
+            if (token == "")
+                return new Expression("Ivaleb");
+
+            if (token == "(")
+            {
+                var result = Parse();
+                if (Parse_token() != ")") return new Expression("Expexted ) , dalba'b");
+                return result;
+            }
+
+            if (Char.IsDigit(token[0]) || token[0] == 'x')
+                return new Expression(token);
+
+            var arg = Parse_simple_expression();
+
+            return new Expression(token, arg);
+        }
+
+        private int Get_priority(string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    return 1;
+                case "-":
+                    return 1;
+                case "*":
+                    return 2;
+                case "/":
+                    return 2;
+                case "^":
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+        Expression Parse_binary_expression(int minPriority)
+        {
+            var leftExpr = Parse_simple_expression();
+
+            while (true)
+            {
+                var op = Parse_token();
+                var priority = Get_priority(op);
+                if (priority <= minPriority)
+                {
+                    _i -= op.Length;
+                    return leftExpr;
+                }
+
+                var rightExpr = Parse_binary_expression(priority);
+                leftExpr = new Expression(op, leftExpr, rightExpr);
+            }
+        }
+
+        public Expression Parse()
+        {
+            return Parse_binary_expression(0);
+        }
+
+        public double Calculate(Expression expr, double x)
+        {
+            switch (expr.Args.Length)
+            {
+                case 2:
+                    var a = Calculate(expr.Args[0], x);
+                    var b = Calculate(expr.Args[1], x);
+                    if (expr.Token == "+") return a + b;
+                    if (expr.Token == "-") return a - b;
+                    if (expr.Token == "*") return a * b;
+                    if (expr.Token == "/") return a / b;
+                    if (expr.Token == "^") return Math.Pow(a, b);
+                    if (expr.Token == "mod") return (int)a % (int)b;
+                    return -1;
+
+                case 1:
+                    var c = Calculate(expr.Args[0], x);
+                    if (expr.Token == "+") return +c;
+                    if (expr.Token == "-") return -c;
+                    if (expr.Token == "abs") return Math.Abs(c);
+                    if (expr.Token == "sin") return Math.Sin(c);
+                    if (expr.Token == "cos") return Math.Cos(c);
+                    return -3;
+
+                case 0:
+                    if (expr.Token == "x")
+                        return (double)x;
+                    return Double.Parse(expr.Token);
+            }
+
+            return -2;
+        }
+    }
+}
